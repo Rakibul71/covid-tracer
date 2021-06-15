10f2c78a90c36d9b27348acf8ac178a9ab2be284
rakib <rakibulislam202019@gmail.com>
2021-06-15 14:58:17 +0600

    1/40/43

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import InfoBox from "./components/InfoBox";
 
 function App() {
+  // https://disease.sh/v3/covid-19/countries
+
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((res) => res.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    // console.log(countryCode);
+    setCountry(countryCode);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">worldwide</MenuItem>
+            {countries.map((country) => {
+              return <MenuItem value={country.value}>{country.name}</MenuItem>;
+            })}
+          </Select>
+        </FormControl>
+      </div>
+      <div className="app__stats">
+        <InfoBox title="Coronavirus Cases"></InfoBox>
+        <InfoBox title="Recovered"></InfoBox>
+        <InfoBox title="Deaths"></InfoBox>
+      </div>
     </div>
   );
 }
